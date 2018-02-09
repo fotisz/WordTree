894bbd3ddefeb04374f1ff0b1ca0f5c8182df889
Fotis <fotisz@users.noreply.github.com>
2018-02-09 12:22:33 +0100

    Update wordtree.js

--- wordtree.js
+++ wordtree.js
@@ -544,11 +544,6 @@ d3.select("#paste-go").on("click", function() {
     url(state = {
         source: ""
     }, !0),
-    d3.select("#paste-save").property("checked") && d3.text("save").post(d3.select("#paste").property("value"), function(e, t) {
-        e ? alert("An error occurred when attempting to save your text for sharing!") : url({
-            source: state.source = t
-        })
-    }),
     processText(d3.select("#paste").property("value"))
 });
 var div = d3.select("#examples").selectAll("div.thumb").data([{
